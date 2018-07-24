14dcd33fa9b6304bb7c6f39e0419099a5c88a9b5
Nilesh Agrawal <niagraw@microsoft.com>
2018-07-24 00:16:05 +0000

    serializing the prediction objects for workitem and areapath so, python endpoints are up and running

--- FlaskService/app.py
+++ FlaskService/app.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, jsonify
 import json
-#import csv
+import csv
 #from Query_Syntactical_Title import loadPickledModels, getsimilar
-from area_classifier.area_classifier import  predict_area
+#from area_classifier.area_classifier import  predict_area
 import logging
 
 logger = logging.getLogger('myapp')
 hdlr = logging.FileHandler('logs.csv')
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
-logger.addHandler(hdlr) 
+logger.addHandler(hdlr)
 logger.setLevel(logging.INFO)
 
 def loadTfidDict():
@@ -44,17 +44,75 @@ def similar_wits():
     #similar, candidates = getsimilar(queryString,model, tfidf)
 
     #response = [buildItem(wit) for wit in similar.values]
-    return json.dumps("deprecated")
+    predictions = getDummyWorkItemPrediction()
+
+    return predictions.toJSON()
 
 import pprint
 @app.route("/area", methods=['POST'])
 def area():
     pprint.pprint(request)
     queryString = request.json['text']
-    prediction = to_full_area_path(predict_area(queryString))
-    logger.info(", "+prediction+", "+queryString)
-    response = {"area":prediction}
-    return json.dumps(response)
+    #prediction = to_full_area_path(predict_area(queryString))
+    #logger.info(", "+prediction+", "+queryString)
+    #response = {"area":prediction}
+    prediction = getDummyAreaPathPrediction()
+    return prediction.toJSON()
+
+
+'''
+Data Contract for area path
+'''
+class Areapathprediction:
+    def __init__(self, areapathname, probability):
+        self.areapathname = areapathname
+        self.probability = probability
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+class Areapathpredictions:
+    def __init__(self):
+        self.predictions = []
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+'''
+Data Contract for workitem.
+'''
+class Workitemprediction:
+    def __init__(self, workitemid, probability):
+        self.workitemid = workitemid
+        self.probability = probability
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+class Workitempredictions:
+    def __init__(self):
+        self.predictions = []
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+'''
+To be removed methods.
+'''
+def getDummyWorkItemPrediction():
+    workitempredictions = Workitempredictions()
+    prediction1 = Workitemprediction(2, 0.75)
+    prediction2 = Workitemprediction(1, 0.65)
+    workitempredictions.predictions.append(prediction1)
+    workitempredictions.predictions.append(prediction2)
+    return  workitempredictions
+
+def getDummyAreaPathPrediction():
+    areapathprediction = Areapathpredictions()
+    apred1 = Areapathprediction("sample", 0.75)
+    apred2 = Areapathprediction("sample", 0.9)
+    areapathprediction.predictions.append(apred1)
+    areapathprediction.predictions.append(apred2)
+    return areapathprediction
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
