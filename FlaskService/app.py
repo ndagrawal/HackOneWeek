2e9de1af3f692710022ae9bcaea64f4042f234a1
Nilesh Agrawal <niagraw@microsoft.com>
2018-07-24 02:19:36 +0000

    adding the webservice / mlpipeline

--- FlaskService/app.py
+++ FlaskService/app.py
@@ -1,15 +1,34 @@
 from flask import Flask, request, jsonify
 import json
 import csv
+from TextCleanUtils import clean_text
+import pickle
+import logging
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import preprocessing
+from sklearn.linear_model import SGDClassifier
+
+#from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn import preprocessing
 #from Query_Syntactical_Title import loadPickledModels, getsimilar
 #from area_classifier.area_classifier import  predict_area
-import logging
+
+
+with open('vso_tfidf.pickle1gram', 'rb') as fw:
+    tfidf = pickle.load(fw)
+
+with open('vso_label_encoder', 'rb') as fw:
+    le = pickle.load(fw)
+
+with open('sgd-all.pickle', 'rb') as fw:
+    model = pickle.load(fw)
 
 logger = logging.getLogger('myapp')
-hdlr = logging.FileHandler('logs.csv')
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-hdlr.setFormatter(formatter)
-logger.addHandler(hdlr)
+#hdlr = logging.FileHandler('logs.csv')
+#formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+#hdlr.setFormatter(formatter)
+#logger.addHandler(hdlr)
+
 logger.setLevel(logging.INFO)
 
 def loadTfidDict():
@@ -42,23 +61,45 @@ def similar_wits():
     #queryString = request.json['text'];
     #logger.info(print("requesting query: "+queryString))
     #similar, candidates = getsimilar(queryString,model, tfidf)
-
     #response = [buildItem(wit) for wit in similar.values]
-    predictions = getDummyWorkItemPrediction()
+    pprint.pprint(request)
+    queryString = request.json['text']
+    result = mlpipeline(queryString)
+    #predictions = getDummyWorkItemPrediction()
+    #return predictions.toJSON()
 
-    return predictions.toJSON()
+    return get_top5(result)
 
 import pprint
 @app.route("/area", methods=['POST'])
 def area():
     pprint.pprint(request)
     queryString = request.json['text']
+    result = mlpipeline(queryString)
+    #print(areapath)
     #prediction = to_full_area_path(predict_area(queryString))
     #logger.info(", "+prediction+", "+queryString)
     #response = {"area":prediction}
-    prediction = getDummyAreaPathPrediction()
-    return prediction.toJSON()
+    #prediction = getDummyAreaPathPrediction()
+    #return prediction.toJSON()
+    return get_top5(result)
 
+
+'''
+Machine Learning Pipeline
+'''
+def mlpipeline(text):
+    return clean_text(text, True)
+
+def get_top5(text):
+    probs = model.predict_proba(tfidf.transform([text]))
+    args = probs.argsort()[0][-5:][::-1]
+    areas, probs = le.classes_[model.classes_[args]], probs[0][args]
+    resultstring = ""
+    for i in range(5):
+        resultstring+=" "+str(areas[i])+" "+str(probs[i])
+        resultstring+='\n'
+    return resultstring
 
 '''
 Data Contract for area path
@@ -112,7 +153,6 @@ def getDummyAreaPathPrediction():
     areapathprediction.predictions.append(apred1)
     areapathprediction.predictions.append(apred2)
     return areapathprediction
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
